51af9da55ff91d54a305ce74cda7a8cd715e69b2
Robert Smith <robert.smith@florey.edu.au>
2025-06-23 20:27:28 +1000

    Ensure Python version is adequate
    Precludes getting an unhandled Exception regarding subscripting of types in Python 3.8 and earler.

--- ipfreely/__init__.py
+++ ipfreely/__init__.py
@@ -1,3 +1,10 @@
+import sys
+
+# Codebase uses subscripted type hints
+if sys.version_info < (3, 9):
+    sys.stderr.write("Requies Python version 3.9 or later\n")
+    sys.exit(1)
+
 class BIDSError(Exception):
     pass
 
